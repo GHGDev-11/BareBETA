4ffe690108b59f4c6ff7aab8f49fdd3613fea76d
GHGDev-11 <67170374+GHGDev-11@users.noreply.github.com>
2020-12-31 18:13:13 +0100

    Add files via upload

---
+++ Compiler.cs
@@ -0,0 +1,105 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Compiler
+{
+
+    class Compiler
+    {
+        static bool tryTrue = false;
+        static bool exceptTrue = true;
+        public string file;
+        bool goodGrammar = true;
+        string ptf = Directory.GetCurrentDirectory();
+        static List<string> keywords = new List<string>();
+        public string csscript = "using System;\n\nnamespace BareRunTime\n{\n    class BareRunTime\n    {\n        static void Main()\n        {";
+        
+
+        public void Compile(string command)
+        {
+            string[] content = Regex.Split(command, "\n");
+            foreach (string line in content)
+            {
+                string[] words = Regex.Split(line, " ");
+                foreach (string word in words)
+                {
+                    if (keywords.Contains(word) && !word.Contains("'") && !word.Contains("\"") && line.StartsWith(word)) ;
+                    {
+                        switch (word)
+                        {
+                            case "say":
+                                Add($"Console.WriteLine({line.Substring(4, line.Length - 4)});");
+                                break;
+                            case "ask":
+                                Add($"Console.Write({line.Substring(4, line.Length - 4)})\nConsole.ReadLine();");
+                                break;
+                            case "make":
+                                string index_joined = String.Join("\\n", content);
+                                Add($"string linecut = \"{line.Substring(5, line.Length - 5)}\";\nstring[] linesplit = Regex.Split(linecut, \":\");\nstring function = linesplit[0];\nstring steps = \"\";\nstring name = function.Substring(1, function.Length - 1);\nstring[] lines_copy = \"{index_joined}\";\nint index = \"{index_joined}\".IndexOf(\"{line}\");");
+                                break;
+                        }
+                    }
+                }
+            }
+            // Execute The script we just compiled from Bare to CS
+        }
+
+        private void exec(string script)
+        {
+            Console.WriteLine(csscript);
+        }
+
+        private void Add(string script)
+        {
+            string[] scriptsplit = Regex.Split(script, "\n");
+            foreach (string scriptline in scriptsplit)
+            {
+                csscript += $"\n            {scriptline}";
+            }
+        }
+
+        public void Finalize()
+        {
+            csscript += "\n        }\n    }\n}";
+            exec(csscript);
+        }
+
+        public void Error(string name, string details, int ln, int wn, string fn)
+        {
+            switch (tryTrue)
+            {
+                case false:
+                    switch (goodGrammar)
+                    {
+                        case false:
+                            Console.WriteLine("\nDuring this error, another error occurred:\n");
+                            break;
+                    }
+                    Console.WriteLine("Error (Most recent is shown last):\n  File '<{fn}>',");
+                    try
+                    {
+                        switch (ln.GetType().ToString())
+                        {
+                            case "string":
+                                Console.WriteLine($"    Line '<lift>', word {wn}:");
+                                break;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"    Line {ln}, word {wn}:");
+                    }
+                    Console.WriteLine($"{name}:\n    {details}");
+                    goodGrammar = false;
+                    tryTrue = true;
+                    break;
+                default:
+                    exceptTrue = true;
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace bpp
+{
+    class Program
+    {
+        static List<string> argv = new List<string>();
+        int st = 0;
+        bool time = false;
+        static System.String run;
+        bool is_string = false;
+
+        static void Main()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            foreach (string arg in args)
+            {
+                argv.Add(arg);
+                //Console.WriteLine($"{arg}: {argv.IndexOf(arg)}");
+            }
+
+            try
+            {
+                string run = argv[1];
+            }
+            catch
+            {
+                Console.WriteLine("Copyright (c) 2020 Bare++.\nAll Rights Reserved.\n\n");
+                // Initialize Compiler
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write(">>> ");
+                        string command = Console.ReadLine();
+                        string end = "NOTHING";
+                        string newlines = "";
+                        if (command.EndsWith(":"))
+                        {
+                            while (end != "")
+                            {
+                                Console.Write("... ");
+                                end = Console.ReadLine();
+                                if (end == "")
+                                {
+                                    break;
+                                }
+                                else
+                                {
+                                    newlines += $"\n{end}";
+                                }
+                            }
+                        }
+                        command += $"\n{newlines}";
+
+                        /*
+                         * Run variable command
+                         * V------------^
+                         * A string the user inputs.
+                         * */
+                    }
+                    catch
+                    {
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+
+            if (argv.Contains("--timeit"))
+            {
+                // Add a timer
+            }
+
+            if (run == "new")
+            {
+                if (argv.Count < 3)
+                {
+                    Console.WriteLine("New file types:\n    Command              Type\n----------------------------------------------------------------------------------------\n    console              Console/Terminal Application (For backend applications, no GUI)\n    conole template      Console/Terminal Application Template (For backend applications, no GUI)\n    window               Windowed Application (For front-end applications, with GUI)\n    window template      Windowed Application Template (For front-end applications, with GUI)");
+                }
+                else
+                {
+                    var type_ = argv[2];
+                    string win_temp = "";
+                    if (argv.Count == 4)
+                    {
+                        win_temp = argv[3];
+                    }
+                    Console.WriteLine($"Creating new file, type: {type_}");
+                    if (type_ == "console" && win_temp == "")
+                    {
+                        System.IO.File.WriteAllText($@"{Directory.GetCurrentDirectory()}\Application.ba", "|| New Console application - generated with Bare");
+                    }
+                    else if (type_ == "window" && win_temp == "")
+                    {
+                        System.IO.File.WriteAllText($@"{Directory.GetCurrentDirectory()}\Application.ba", "||  Imports / Uses\nuse <bagui> -> [built-in]\n\n|| Window Initialization\nnew-window 'root'\n\n|| Window - Start Looping Frames\nloop 'root'");
+                    }
+                    else if (type_ == "window" && win_temp == "template")
+                    {
+                        System.IO.File.WriteAllText($@"{Directory.GetCurrentDirectory()}\Application.ba", "||  Imports / Uses\nuse <bagui> -> [built-in]\n\n|| Window Initialization\nnew-window 'root'\n\n|| Window Configuration\nconfigure 'root' ; background-color='#282828' ; title='Application'\n\n|| Add Widgets To Window\nadd-text 'root' ; label='Label_1' ; background-color='#282828' ; foreground-color='white' ; text='Test Application' ; font='Haettenschweiler' ; font-size=65 ; font-weight='normal'\nadd-space 'root' ; '#282828'\nadd-text 'root' ; label='Login' ; background-color='#282828' ; foreground-color='white' ; text='Login' ; font='Haettenschweiler' ; font-size=15 ; font-weight='normal'\nadd-space 'root' ; '#282828'\nadd-inputfield 'root' ; label='Username' ; text='Username'\nadd-inputfield 'root' ; label='Password' ; text='Password'\nmake $checklogin: get-response 'Username'=uname | get-response 'Password'=upass | say 'Username:' | say uname | say '\\\\n' | say 'Password:' | say upass\nadd-space 'root' ; '#282828'\nadd-button 'root' ; label='LoginButton' ; width=25 ; command=$checklogin ; text='Login' ; border=0\n\n|| Window - Start Looping Frames\nloop 'root'");
+                    }
+                    else if (type_ == "console" && win_temp == "template")
+                    {
+                        System.IO.File.WriteAllText($@"{Directory.GetCurrentDirectory()}\Application.ba", "|| New Console application - generated with Bare\n\nmake $main:\n    say 'Hello, World!'\n    return 0\n\nif null == 0:\n    $main");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: BPP001: Type '{type_}' does not exist.");
+                    }
+
+                }
+            }
+            RunNormal();
+        }
+
+        public static void RunCompiled()
+        {
+            // Run Compiled
+        }
+
+        public static void RunNormal()
+        {
+            var compiler = new Compiler.Compiler();
+            compiler.Compile(Console.ReadLine());
+            compiler.Finalize();
+        }
+    }
+}
